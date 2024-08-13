453c14374b46bab7c87762f68843eb0129a1c496
Y.Ko <kyc881201@gmail.com>
2024-08-13 11:10:23 +0800

    240813 完成

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -14,13 +14,22 @@ class App extends Component {
 
   fetchData = async () => {
     try {
-      const response = await fetch( ... 略 ... );
+      const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2V4YW1wbGUuYXV0aDAuY29tLyIsImF1ZCI6Imh0dHBzOi8vYXBpLmV4YW1wbGUuY29tL2NhbGFuZGFyL3YxLyIsInN1YiI6InVzcl8xMjMiLCJpYXQiOjE0NTg3ODU3OTYsImV4cCI6MTQ1ODg3MjE5Nn0.CA7eaHjIHz5NxeIJoFK9krqaeZrPLwmMmgI_XiQiIkQ'
+      const response = await fetch('http://140.127.196.78:8344/sensor', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${key}`
+        },
+        body: JSON.stringify({ authorization: `Bearer ${key}` }) // 添加請求體
+      });
 
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
 
       const data = await response.json();
+      console.log('Fetched data:', data); // 檢查回應的資料
       this.setState({ data });
     } catch (error) {
       console.error('Fetch error:', error);
